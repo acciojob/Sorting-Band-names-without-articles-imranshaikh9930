3386440e49e474f2eeb9dd2b3e3e5b9979ddf3f1
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-11 21:50:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here
 
+const bandNames = [
+  'The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'
+];
+function sortBandNames(bandNames) {
+  return bandNames.sort((a, b) => {
+      const articles = /^(a |an |the )/i;
+      const nameA = a.replace(articles, "");
+      const nameB = b.replace(articles, "");
+      return nameA.localeCompare(nameB);
+  });
+}
+
+// Get the ul element
+const ulElement = document.getElementById('band');
+
+// Sort and display the band names
+const sortedBandNames = sortBandNames(bandNames);
+for (const name of sortedBandNames) {
+  const liElement = document.createElement('li');
+  liElement.textContent = name;
+  ulElement.appendChild(liElement);
+}
